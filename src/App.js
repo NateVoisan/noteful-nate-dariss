035d9854d6ff948cf0f83ef0f4c07c4de2c41220
Courtney Carson <cmacarson@gmail.com>
2020-10-06 11:57:46 -0700

    dcomponents updates

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom';
+import MainPage from './Main/MainPage';
+import AddNote from './AddNote/AddNote';
+import FolderNavs from './FolderNavs/FolderNavs';
 
 function App() {
   return (
     <main className='App'>
+      <Route
+        exact Path='/'
+        component={MainPage}
+      />
+      <Route
+        path='/AddNote/:AddNoteId'
+        component={AddNote}
+      />
+      <Route
+        path='/FolderNavs/:FolderNavsId'
+        component={FolderNavs}
+      />
       {/* content goes here */}
     </main>
   );
--- src/Main/MainPage.js
+++ src/Main/MainPage.js
@@ -1,24 +1,30 @@
 import React from 'react'
 import './MainPage.css'
 
+
 export default function MainPage() {
   return (
+
     <div className="flex-container">
       <div className="back-container">
         <nav className="back-nav"><button type="submit" for="back-nav" id="back-nav">Back</button></nav>
       </div>
-      <div className="note-container">
-        <div className="note">
-          <fieldset>
-            <legend><h2 className="title">Note 1</h2></legend>
-            <p className="date">Date modified on 6th October 2020</p>
-            <button type="delete" for="delete-button" id="delete-button">Delete Note</button>
-          </fieldset>
-        </div>
-      </div>
-      <div className="note-description-container">
-        <p className="note-description">Lorem ipsum blah blah description stuff blah blah</p>
-      </div>
+
+      <ul>
+        <li>
+          <div className="note-container">
+            <div className="note">
+              <fieldset>
+                <link to='/'
+                <h2> className="title">Note 1</h2>
+                <p className="date">Date modified on 6th October 2020</p>
+                <button type="delete" for="delete-button" id="delete-button">Delete Note</button>
+              </fieldset>
+            </div>
+          </div>
+        </li>
+      </ul>
+
     </div>
   )
 }
